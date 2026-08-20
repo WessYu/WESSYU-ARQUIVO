2197c5515e94be08c69904521c56f039ba804b39
Wesley <wess.c@proton.me>
2026-08-20 16:23:41 -0300

    fix: restore portfolio-first experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ReferencePortfolio from './ReferencePortfolio'
+import App from './App'
 import './styles.css'
-import './reference-portfolio.css'
 import { ErrorBoundary } from './components/ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <ReferencePortfolio />
+      <App />
     </ErrorBoundary>
   </React.StrictMode>
 )
