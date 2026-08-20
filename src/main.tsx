caef077316a04642d86b3a3f5c3f13b77c71fc15
Wesley <wess.c@proton.me>
2026-08-20 15:52:26 -0300

    feat: switch portfolio to video-reference experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReferencePortfolio from './ReferencePortfolio'
 import './styles.css'
-import './refinements.css'
-import './tech-stack.css'
-import './project-reel.css'
-import './project-reel-details.css'
-import './component-vault-spotlight.css'
-import './portfolio-polish.css'
-import './performance.css'
-import './studio-direction.css'
-import './studio-finish.css'
-import './professional-experience.css'
+import './reference-portfolio.css'
 import { ErrorBoundary } from './components/ErrorBoundary'
-import { TechStack } from './components/TechStack'
-import { mountComponentVaultSpotlight } from './componentVaultSpotlight'
-import { initProjectReel } from './projectReel'
-import { applyContentEnhancements } from './contentEnhancements'
-import { initProfessionalExperience } from './professionalExperience'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
-      <TechStack />
+      <ReferencePortfolio />
     </ErrorBoundary>
   </React.StrictMode>
 )
-
-const startEnhancements = () => {
-  mountComponentVaultSpotlight()
-  initProjectReel()
-  applyContentEnhancements()
-  initProfessionalExperience()
-}
-
-if ('requestIdleCallback' in window) {
-  window.requestIdleCallback(startEnhancements, { timeout: 900 })
-} else {
-  window.setTimeout(startEnhancements, 120)
-}
